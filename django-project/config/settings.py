91e2f160d8fcac8ce018a4ddde9f38d08801e7f6
ben <benmo009@gmail.com>
2023-01-26 23:50:02 -0800

    Change leaked secret key

--- django-project/config/settings.py
+++ django-project/config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ok1mi$fc#61@#^u!)@oh+6p&^sd!gasrgehs2gmmg$wm99se*('
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
