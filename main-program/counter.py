80a91786364aae59875699fff1c1b43c468063b5
ben <benmo009@gmail.com>
2023-01-26 23:50:15 -0800

    Add separate counter program

---
+++ main-program/counter.py
@@ -0,0 +1,18 @@
+import time
+
+class Counter:
+    def __init__(self, max_count: int, current_count: int = 0):
+        self.max_count = max_count
+        self.current_count = current_count
+
+    def run(self):
+        print(self.current_count)
+        while (self.current_count < self.max_count):
+            self.current_count += 1
+            print(self.current_count)
+            time.sleep(0.5)
+
+
+if __name__ == "__main__":
+    counter = Counter(max_count=20)
+    counter.run()
