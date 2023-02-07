7c9f594cf1564624395083adc8c3240002ef56e0
ben <benmo009@gmail.com>
2023-02-06 22:07:19 -0800

    Implement Redis channel layer

--- django-project/config/settings.py
+++ django-project/config/settings.py
@@ -70,6 +70,14 @@ TEMPLATES = [
     },
 ]
 ASGI_APPLICATION = 'config.asgi.application'
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
 WSGI_APPLICATION = 'config.wsgi.application'
 
 
--- django-project/counter/consumers.py
+++ django-project/counter/consumers.py
@@ -1,17 +1,41 @@
 # counter/consumers.py
 import json
 
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
-class CountConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
+class CountConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.counter_name = self.scope['url_route']['kwargs']['counter_id']
+        self.counter_group = f'counter_{self.counter_name}'
 
-    def disconnect(self, close_code):
-        pass
+        await self.channel_layer.group_add(self.counter_group, self.channel_name)
+        
+        await self.accept()
 
-    def receive(self, text_data):
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(self.counter_group, self.channel_name)
+
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
 
-        self.send(text_data=json.dumps({"message": message}))
+        if text_data_json["type"] == "start":
+            response = { "type": "start_ack" }
+        elif text_data_json["type"] == "pause":
+            response = { "type": "pause_ack" }        
+
+        await self.channel_layer.group_send(
+            self.counter_group, response
+        )
+
+    async def start_ack(self, event):
+        event["message"] = "starting count"
+
+        await self.send(text_data=json.dumps(event))
+
+    async def pause_ack(self, event):
+        event["message"] = "pausing count"
+
+        await self.send(text_data=json.dumps(event))
+
+    async def test(self, event):
+        await self.send(text_data=json.dumps(event))
